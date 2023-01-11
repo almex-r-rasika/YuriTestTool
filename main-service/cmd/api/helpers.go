f5fca765c6f1a89751636b8ac6be2ee788c91e17
r-rasika@usen-next.jp <r-rasika@usen-next.jp>
2023-01-11 17:11:47 +0900

    added function to write json response to client

--- main-service/cmd/api/helpers.go
+++ main-service/cmd/api/helpers.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"service/data"
@@ -103,4 +105,33 @@ func getSendMessageJsonList() []SendMessageLog{
 		logList = append(logList, log)
 	}
 	return logList
+}
+
+// writeJSON takes a response status code and arbitrary data and writes a json response to the client
+/* function for writes a json response to the client
+   @param --> http.ResponseWriter, int, any, http.Header
+   @param value --> w http.ResponseWriter, status int, data any, headers ...http.Header
+   description --> writeJSON takes a response status code and arbitrary data and writes a json response to the client
+   @return --> error
+*/
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
