6ac9e78d4471e56dc6b580087b3c28ccf9769c87
r-rasika@usen-next.jp <r-rasika@usen-next.jp>
2023-01-11 16:18:27 +0900

    removed comments

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -32,10 +32,6 @@ func render(w http.ResponseWriter, t string) {
 
 	templateSlice = append(templateSlice, partials...)
 
-	//for _, x := range partials {
-		//templateSlice = append(templateSlice, x)
-	//}
-
 	tmpl, err := template.ParseFiles(templateSlice...)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
